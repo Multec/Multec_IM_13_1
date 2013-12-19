adde3684acdae509694e5eea52dae0865dd8be7a
jensvervaeren <jensvervaeren@msn.com>
2013-12-19 16:47:42 +0100

    Test

---
+++ src/Trol.java
@@ -0,0 +1,22 @@
+/**
+ * @author jensvervaeren
+ *
+ */
+public class Trol {
+
+	/**
+	 * 
+	 */
+	public Trol() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
