f0018c66f862f741e89d32b360b7b98a8393e8c1
Arno Jansens <arno.jansens@telenet.be>
2013-12-19 16:51:30 +0100

    Test Arno

---
+++ src/Test_Arno.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author arnojansens
+ *
+ */
+public class Test_Arno {
+
+	/**
+	 * 
+	 */
+	public Test_Arno() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
